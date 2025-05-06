2df35b5713e27a197765461f22784aa39d55d64f
itency <itency@outlook.com>
2025-05-06 13:16:58 +0300

    add method run and registr

--- command.go
+++ command.go
@@ -1,6 +1,25 @@
 package main
 
+import "fmt"
+
 type Command struct {
 	name string
 	args []string
+}
+
+type Commands struct {
+	validCommands map[string]func(*State, Command) error
+}
+
+func (c *Commands) run(s *State, cmd Command) error {
+	cmds, ok := c.validCommands[cmd.name]
+	if !ok {
+		return fmt.Errorf("invalid command")
+	}
+
+	return cmds(s, cmd)
+}
+
+func (c *Commands) register(name string, f func(*State, Command) error) {
+	c.validCommands[name] = f
 }
